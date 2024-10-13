0cabcfbf06d5e56749568c66184ff54924b75334
MullerEsposito <mulleresposito@hotmail.com>
2024-10-13 16:15:50 -0300

    events(NFTMintedTo, NFTCreated, NFTUpdated): it creates 3 new events that are auto describle;

--- contracts/events.sol
+++ contracts/events.sol
@@ -2,4 +2,7 @@
 pragma solidity ^0.8.20;
 
 event NFTMinted(uint8 idxNFT, uint8 idToken, address to);
+event NFTMintedTo(uint8 idxNFT, uint8 idToken, address from, address to);
 event NFTTransfered(uint8 idToken, address from, address to);
+event NFCCreated(uint16 numberOfNFTs, string _baseURI);
+event NFCUpdated(uint16 idxNFT);
