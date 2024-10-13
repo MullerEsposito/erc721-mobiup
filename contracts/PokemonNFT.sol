a7fe654ed7cfee757b78576d532eb32c00d87639
MullerEsposito <mulleresposito@hotmail.com>
2024-10-13 18:52:48 -0300

    refact(Royalty): it update the royalty logic to pay the fee royalties for the original minter of the token;

--- contracts/PokemonNFT.sol
+++ contracts/PokemonNFT.sol
@@ -6,92 +6,26 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./structs/NFT.sol";
 import "./structs/MintParams.sol";
+import "./structs/TokenData.sol";
 import "./errors.sol";
 import "./events.sol";
 
 contract PokemonNFT is ERC721URIStorage, Ownable {
     mapping (uint16 => NFT) public nfts;
-    mapping (uint8 => uint8) private _mapTokenIdNFTType;
+    mapping (uint8 => TokenData) private _mapTokenIdTokenData;
     mapping (uint8 => uint8) private _mapTokenIdPositionInType;
 
-    address public royaltyRecipient;
     string public contractURI;
     uint256 public royaltyFee;
     uint8 public nextTokenId = 1;
     uint16 public numberOfNFTs = 0;
 
-    constructor(string memory _contractURI, address _royaltyRecipient, uint256 _royaltyFee, string memory _name, string memory _symbol) 
+    constructor(string memory _contractURI, uint256 _royaltyFee, string memory _name, string memory _symbol) 
         ERC721(_name, _symbol)
         Ownable(msg.sender)
     {
         contractURI = _contractURI;
-        royaltyRecipient = _royaltyRecipient;
         royaltyFee = _royaltyFee;
-    }
-
-    function _mintVerifications(uint8 _typeNFT) internal returns (uint8) {
-        if (_typeNFT > numberOfNFTs || _typeNFT == 0) revert InexistentNFT(_typeNFT);
-
-        NFT storage nft = nfts[_typeNFT];
-        
-        if (nft.maxSupply == 0) revert SupplyUnavailable();
-        if (nft.currentSupply >= nft.maxSupply) revert SupplyExceeded(nft);
-        if (msg.value != nft.mintPrice) revert WrongPaymentValue(nft.mintPrice, msg.value);
-        
-        uint8 tokenId = nextTokenId++;
-        nft.currentSupply += 1;
-        _mapTokenIdNFTType[tokenId] = _typeNFT;
-        _mapTokenIdPositionInType[tokenId] = nft.currentSupply;
-
-        return tokenId;
-    }
-
-    function _generateTokenURI(uint8 _tokenId) internal view returns (string memory) {
-        uint8 typeNFT = _mapTokenIdNFTType[_tokenId];
-        NFT storage nft = nfts[typeNFT];
-        uint8 tokenNumber = _mapTokenIdPositionInType[_tokenId];
-
-        return string(abi.encodePacked(nft.baseURI, Strings.toString(tokenNumber), ".json"));
-    }
-
-    function mintNFT(MintParams memory _mintParams) public payable{
-        uint8 tokenId = _mintVerifications(_mintParams.typeNFT);
-        string memory tokenURI = _generateTokenURI(tokenId);
-
-        if (_mintParams.to == address(0)) {
-            _safeMint(msg.sender, tokenId);
-        } else {
-            _safeMint(_mintParams.to, tokenId);            
-        }
-        _setTokenURI(tokenId, tokenURI);
-        
-        emit NFTMinted(_mintParams.typeNFT, tokenId, msg.sender);
-    }
-
-    function sellNFT(address to, uint8 tokenId) public payable {
-        uint256 salePrice = msg.value;
-        uint256 royaltyValue = (salePrice * royaltyFee) / 100;
-
-        if (msg.sender != ownerOf(tokenId)) revert IsNotNFTOwner();
-
-        bool success = payable(royaltyRecipient).send(royaltyValue);
-        if (!success) revert FailedToPayRoyalty();
-
-        success = payable(msg.sender).send(salePrice - royaltyValue);
-        if (!success) revert FailedToPaySale();
-
-        _transfer(msg.sender, to, tokenId);
-
-        emit NFTTransfered(tokenId, msg.sender, to);
-    }
-
-    function updateContractURI(string memory _contractURI) public {
-        contractURI = _contractURI;
-    }
-
-    function withDraw() public onlyOwner {
-        (bool success, ) = payable(owner()).call{value: address(this).balance}("");
-        if (!success) revert TransferFailed();
     }
 
     function createNFT(uint8 _maxSupply, uint256 _mintPrice, string calldata _baseURI) public onlyOwner {
@@ -118,4 +52,78 @@ contract PokemonNFT is ERC721URIStorage, Ownable {
 
         emit NFCUpdated(_typeNFT);
     }
+
+    function mintNFT(MintParams memory _mintParams) public payable{
+        uint8 tokenId = _mintVerifications(_mintParams.typeNFT);
+        string memory tokenURI = _generateTokenURI(tokenId);
+
+        if (_mintParams.to == address(0)) {
+            _safeMint(msg.sender, tokenId);
+            _mapTokenIdTokenData[tokenId].royaltyAddress = msg.sender;
+
+        } else {
+            _safeMint(_mintParams.to, tokenId);
+            _mapTokenIdTokenData[tokenId].royaltyAddress = _mintParams.to;
+        }
+        _setTokenURI(tokenId, tokenURI);
+        
+        emit NFTMinted(_mintParams.typeNFT, tokenId, msg.sender);
+    }
+
+    function _mintVerifications(uint8 _typeNFT) internal returns (uint8) {
+        if (_typeNFT > numberOfNFTs || _typeNFT == 0) revert InexistentNFT(_typeNFT);
+
+        NFT storage nft = nfts[_typeNFT];
+        
+        if (nft.maxSupply == 0) revert SupplyUnavailable();
+        if (nft.currentSupply >= nft.maxSupply) revert SupplyExceeded(nft);
+        if (msg.value != nft.mintPrice) revert WrongPaymentValue(nft.mintPrice, msg.value);
+        
+        uint8 tokenId = nextTokenId++;
+        nft.currentSupply += 1;
+        _mapTokenIdTokenData[tokenId].nftType = _typeNFT;
+        _mapTokenIdPositionInType[tokenId] = nft.currentSupply;
+
+        return tokenId;
+    }
+
+    function _generateTokenURI(uint8 _tokenId) internal view returns (string memory) {
+        TokenData storage tokenData = _mapTokenIdTokenData[_tokenId];
+        NFT storage nft = nfts[tokenData.nftType];
+        uint8 tokenNumber = _mapTokenIdPositionInType[_tokenId];
+
+        return string(abi.encodePacked(nft.baseURI, Strings.toString(tokenNumber), ".json"));
+    }
+    
+    function sellNFT(address to, uint8 tokenId) public payable {
+        uint256 salePrice = msg.value;
+        uint256 royaltyValue = (salePrice * royaltyFee) / 100;
+
+        if (msg.sender != ownerOf(tokenId)) revert IsNotNFTOwner();
+
+        TokenData storage tokenData = _mapTokenIdTokenData[tokenId];
+        
+        bool success = payable(tokenData.royaltyAddress).send(royaltyValue);
+        if (!success) revert FailedToPayRoyalty();
+
+        success = payable(msg.sender).send(salePrice - royaltyValue);
+        if (!success) revert FailedToPaySale();
+
+        _transfer(msg.sender, to, tokenId);
+
+        emit NFTTransfered(tokenId, msg.sender, to);
+    }
+
+    function updateContractURI(string memory _contractURI) public {
+        contractURI = _contractURI;
+    }
+
+    function balance() public view returns (uint256) {
+        return address(this).balance;
+    }
+    
+    function withDraw() public onlyOwner {
+        (bool success, ) = payable(owner()).call{value: address(this).balance}("");
+        if (!success) revert TransferFailed();
+    }    
 }
---
+++ contracts/structs/TokenData.sol
@@ -0,0 +1,7 @@
+//SPDX-Licence_Identifier: MIT
+pragma solidity ^0.8.20;
+
+struct TokenData {
+    uint8 nftType;
+    address royaltyAddress;
+}
